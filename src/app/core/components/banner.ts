4e6ad51eef835563b5455772bb6bf36438d81e5a
Greg Wilson <gregory.wilson@wilsongp.net>
2017-10-29 13:28:53 -0500

    Updated banner

--- src/app/core/components/banner.ts
+++ src/app/core/components/banner.ts
@@ -3,9 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-banner',
   template: `
-  <mat-card  fxLayout="column" fxLayoutAlign="center center"  fxLayoutGap="1em">
+  <mat-card  fxLayout="column" fxLayoutAlign="center center" >
     <mat-card-content>
       <p>Greg Wilson</p>
+      <span>Software Engineer</span>
     </mat-card-content>
   </mat-card>
   `,
