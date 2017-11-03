b8c2cc856fe15a4776f3d8e2d8b9079ee9470194
Greg Wilson <gregory.wilson@wilsongp.net>
2017-11-03 17:44:33 -0500

    Working on banner styles

--- src/app/core/components/banner.ts
+++ src/app/core/components/banner.ts
@@ -3,11 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-banner',
   template: `
-  <div class="banner" fxLayout="column wrap" fxLayoutAlign="center center">
-    <div class="banner-content" fxFill>
+  <div class="banner" fxLayout="row wrap" fxLayoutAlign="start center">
+    <div class="banner-content" fxFlex="100" fxFlex.sm="40" fxFlex.md="50" fxFlex.gt-md="65">
       <p>Greg Wilson</p>
       <span>Software Engineer</span>
     </div>
+
   </div>
   `,
   styleUrls: ['./banner.scss']
