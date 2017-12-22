0d8c6223a6ddf0cfdf86d9ae9c131615b30d45de
Greg Wilson <greg.wilson@wilsongp.net>
2017-12-22 09:01:16 -0600

    Updated style for screen sizes

--- src/app/home/components/hobby.ts
+++ src/app/home/components/hobby.ts
@@ -16,7 +16,7 @@ import { Hobby } from '../models/hobby';
   selector: 'app-hobby',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div [@scrollAnimation]="state" fxLayoutAlign="space-between center">
+    <div fxLayoutAlign="space-between center">
       <img [src]="hobby.imageUrl" alt="Hobby image">
       <div fxLayout="column" fxLayoutAlign="space-around center" fxFlex fxHide.lt-md="true">
           <div>
@@ -27,44 +27,9 @@ import { Hobby } from '../models/hobby';
       <ng-content></ng-content>
     </div>
   `,
-  styleUrls: ['../containers/hobbies/hobbies.component.scss'],
-  animations: [
-      trigger('scrollAnimation', [
-        state('dim', style({
-          filter: "brightness(.5)",
-          "z-index": 3
-        })),
-        state('brighten',   style({
-          filter: "brightness(1)",
-          "z-index": 1
-        })),
-        transition('dim => brighten', animate('700ms ease-out')),
-        transition('brighten => dim', animate('700ms ease-in'))
-      ])
-  ]
+  styleUrls: ['../containers/hobbies/hobbies.component.scss']
 })
 export class HobbyComponent {
   @Input() hobby: Hobby;
-
-  state = 'brighten';
-  filterClass = false;
-
-  constructor(public el: ElementRef) { }
-
-    @HostListener('window:scroll', ['$event'])
-      checkScroll() {
-        const thisHeight = this.el.nativeElement.offsetHeight;
-        const componentPosition = this.el.nativeElement.offsetTop;
-        const scrollPosition = window.pageYOffset;
-
-        console.log(thisHeight, componentPosition, scrollPosition);
-
-        if (scrollPosition + thisHeight >= componentPosition) {
-          this.state = 'dim';
-        } else {
-          this.state = 'brighten';
-        }
-
-  }
 
 }
